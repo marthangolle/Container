e5a17d9a4a3d052d35179a60558a7dc3aa5e5ae0
marthangolle <marthangolle23@gmail.com>
2024-09-15 17:56:33 +0200

    Add files via upload

---
+++ NFCStatus.java
@@ -0,0 +1,13 @@
+package com.example.rfideinlessen;
+
+public enum NFCStatus {
+    NoOperation,
+    Tap,
+    Process,
+    Confirmation,
+    Read,
+    Write,
+    NotSupported,
+    NotEnabled,
+}
+
